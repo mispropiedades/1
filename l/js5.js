c3bb20cadcb5d919fe182ef55a7a6067c8cca51b
mispropiedades <oficina046b@outlook.com>
2026-05-12 16:02:47 -0400

    Update js5.js

--- l/js5.js
+++ l/js5.js
@@ -9,7 +9,7 @@ L.marker([-32.93939, -71.27859])
                  class="popup-image">
 
             <ul>
-                <li><span class="emoji">🏠</span> 8750 m2</li>
+                <li><span class="emoji">🟫</span> 8750 m2</li>
                 <li><span class="emoji">🚌</span> a 20 min</li>
             </ul>
         </div>
