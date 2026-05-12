ea92b8e1d748394d8c2a56c6fcde00bc74002fa5
mispropiedades <oficina046b@outlook.com>
2026-05-12 16:02:28 -0400

    Update js4.js

--- l/js4.js
+++ l/js4.js
@@ -5,11 +5,11 @@ L.marker([-32.68410, -71.22076])
             <h3>Propiedad 2</h3>
 
             <img src="../f/pic3.jpg"
-                 alt="Propiedad 2"
+                 alt="Propiedad 3"
                  class="popup-image">
 
             <ul>
-                <li><span class="emoji">🏠</span> 8750 m2</li>
+                <li><span class="emoji">🟫</span> 5750 m2</li>
                 <li><span class="emoji">🚌</span> a 20 min</li>
             </ul>
         </div>
