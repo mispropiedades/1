089d38ce4df0270b3181e9c47c7802ea27598de0
mispropiedades <oficina046b@outlook.com>
2026-05-12 15:32:05 -0400

    Create js5.js

---
+++ l/js5.js
@@ -0,0 +1,16 @@
+L.marker([-32.93939, -71.27859])
+    .addTo(map)
+    .bindPopup(`
+        <div class="popup-content">
+            <h3>Propiedad 2</h3>
+
+            <img src="../f/pic4.jpg"
+                 alt="Propiedad 2"
+                 class="popup-image">
+
+            <ul>
+                <li><span class="emoji">🏠</span> 8750 m2</li>
+                <li><span class="emoji">🚌</span> a 20 min</li>
+            </ul>
+        </div>
+    `);
