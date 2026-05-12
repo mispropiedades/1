37f508974ada7f5ec74712c45885408e394d54ad
mispropiedades <oficina046b@outlook.com>
2026-05-12 16:54:40 -0400

    Update js2.js

--- l/js2.js
+++ l/js2.js
@@ -1,3 +1,4 @@
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+const heroImage = document.getElementById("heroImage");
