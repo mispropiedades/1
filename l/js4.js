c0e985075b11f04edaf340857a771bcbd52fcd69
mispropiedades <oficina046b@outlook.com>
2026-05-12 14:59:13 -0400

    Update js4.js

--- l/js4.js
+++ l/js4.js
@@ -4,7 +4,7 @@ L.marker([-32.68410, -71.22076])
         <div class="popup-content">
             <h3>Propiedad 2</h3>
 
-            <img src="f/pic3.jpg"
+            <img src="../f/pic3.jpg"
                  alt="Propiedad 2"
                  class="popup-image">
 
