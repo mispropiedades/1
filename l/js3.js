179347bcab36f1ec19f1dcd9728d635a7456dda3
mispropiedades <oficina046b@outlook.com>
2026-05-12 14:53:53 -0400

    Create js3.js

---
+++ l/js3.js
@@ -0,0 +1,20 @@
+L.marker([-33.0019, -71.2657])
+    .addTo(map)
+    .bindPopup(`
+        <div class="popup-content">
+            <h3>Propiedad 1</h3>
+
+            <img src="f/pic2.jpg"
+                 alt="Propiedad 1"
+                 class="popup-image">
+
+            <ul>
+                <li><span class="emoji">🏠</span> 1750 m2</li>
+                <li><span class="emoji">🛏️</span> x3</li>
+                <li><span class="emoji">🚿</span> x2</li>
+                <li><span class="emoji">🚝</span> a 20 min</li>
+                <li><span class="emoji">🚌</span> a 5 min</li>
+                <li><span class="emoji">🛒</span> a 5 min</li>
+            </ul>
+        </div>
+    `);
