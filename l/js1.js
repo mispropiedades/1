c942f8b0fa0624ce28408cd177dfbe9d6cb51e21
mispropiedades <oficina046b@outlook.com>
2026-05-13 16:40:25 -0400

    Update js1.js

--- l/js1.js
+++ l/js1.js
@@ -1,1 +1,21 @@
 const map = L.map('map').setView([-32.8000, -71.2367], 10);
+const galleries = {
+    pic2: [
+        "../f/pic2.jpg",
+        "../f/pic9.jpg",
+    ],
+
+    pic3: [
+        "../f/pic3.jpg",
+        "../f/pic7.jpg",
+    ],
+
+    pic4: [
+        "../f/pic4.jpg",
+        "../f/pic8.jpg",
+    ],
+    pic5: [
+      "../f/pic5.jpg",
+      "../f/pic6.jpg",
+    ]
+};
