e0f9a7e4d863c075daa22e7cc7b983ac7ae38cdb
mispropiedades <oficina046b@outlook.com>
2026-05-13 16:52:56 -0400

    Update js2.js

--- l/js2.js
+++ l/js2.js
@@ -2,3 +2,34 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 const heroImage = document.getElementById("heroImage");
+function updateHeroImage() {
+    heroImage.src = currentGallery[currentImageIndex];
+}
+
+
+//
+nextBtn.addEventListener("click", function () {
+
+    if (currentGallery.length === 0) return;
+
+    currentImageIndex++;
+
+    if (currentImageIndex >= currentGallery.length) {
+        currentImageIndex = 0;
+    }
+
+    updateHeroImage();
+});
+
+prevBtn.addEventListener("click", function () {
+
+    if (currentGallery.length === 0) return;
+
+    currentImageIndex--;
+
+    if (currentImageIndex < 0) {
+        currentImageIndex = currentGallery.length - 1;
+    }
+
+    updateHeroImage();
+});
