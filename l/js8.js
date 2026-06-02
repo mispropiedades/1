9de68cf0ebbfcd9116ff4cd6c34b92d6ef05e0c3
mispropiedades <oficina046b@outlook.com>
2026-06-02 16:36:28 -0400

    Create js8.js

---
+++ l/js8.js
@@ -0,0 +1,22 @@
+// 1. Create a Layer Group for your high-zoom elements
+const highZoomGroup = L.layerGroup().addTo(map);
+
+// 2. Add your second text marker directly to this group
+L.marker(targetCoords, {
+    icon: L.divIcon({
+        className: 'yellow-map-text-sub',
+        html: 'Your Text Here2',
+        iconSize: [100, 20],
+        iconAnchor: [50, 2]
+    })
+}).addTo(highZoomGroup);
+
+// 3. Set the minimum zoom for this entire group natively
+// Leaflet will automatically hide/show it based on this rule
+map.on('zoomend', function() {
+    if (map.getZoom() >= 13) {
+        if (!map.hasLayer(highZoomGroup)) map.addLayer(highZoomGroup);
+    } else {
+        if (map.hasLayer(highZoomGroup)) map.removeLayer(highZoomGroup);
+    }
+});
